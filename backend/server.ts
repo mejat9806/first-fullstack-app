6f08ab18d6046be9c45dfd16cad58da711032847
mejat9806 <110531287+mejat9806@users.noreply.github.com>
2024-05-28 22:03:37 +0800

    update

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+const port = process.env.PORT || 8000;
 
 console.log(process.env.MONGO_URL);
 mongoose
   .connect(process.env.MONGO_URL as string)
   .then(() => console.log("db connection established"))
   .catch((err: any) => console.log("db connection fail", err));
-const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`server is running on ${port}`);
 });

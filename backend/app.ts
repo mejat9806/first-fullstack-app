52e4b3ee28b8c67270f405efb9b14f6ab7c4b0c0
mejat9806 <110531287+mejat9806@users.noreply.github.com>
2024-07-01 05:04:24 +0800

    update

--- backend/app.ts
+++ backend/app.ts
@@ -25,7 +25,7 @@ import { fileURLToPath } from "url";
 import { router as likeRouter } from "./routes/likeRoute.js";
 dotenv.config();
 const corsOptions = {
-  origin: "https://socialmedia-650u.onrender.com/",
+  origin: "https://socialmedia-650u.onrender.com",
   // origin: "http://localhost:5173",
   // Allow requests from this origin
   methods: ["GET", "POST", "DELETE", "PUT", "PATCH"], // Allow GET and POST requests

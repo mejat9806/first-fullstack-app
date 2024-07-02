7ec40696a60460239a25c380bf257e5d92a5046f
mejat9806 <110531287+mejat9806@users.noreply.github.com>
2024-07-03 07:03:52 +0800

    update multer

--- backend/dist/backend/utils/multerMultiImage.js
+++ backend/dist/backend/utils/multerMultiImage.js
@@ -1,51 +1,57 @@
 import multer from "multer";
 import sharp from "sharp";
 import { v4 as uuidv4 } from "uuid";
-import { catchAsync } from "./catchAsync";
-import { AppError } from "./appError";
-import cloudinarysetup from "./cloudinary";
+import { catchAsync } from "./catchAsync.js";
+import { AppError } from "./appError.js";
+import cloudinarysetup from "./cloudinary.js";
 const multerStorage = multer.memoryStorage();
 const multerFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith("image")) {
-        cb(null, true);
-    }
-    else {
-        cb(AppError("Please upload an image file only", 400), false);
-    }
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(AppError("Please upload an image file only", 400), false);
+  }
 };
 const upload = multer({
-    storage: multerStorage,
-    fileFilter: multerFilter,
+  storage: multerStorage,
+  fileFilter: multerFilter,
 });
 export const uploadPostImage = upload.fields([{ name: "image", maxCount: 4 }]);
 export const resizePostImage = catchAsync(async (req, res, next) => {
-    const files = req.files;
-    if (!files || !files.image) {
-        return next();
-    }
-    req.body.image = [];
-    req.body.imagePublicIds = [];
-    await Promise.all(files.image.map(async (image, i) => {
-        const fileName = `post-${req.user.id}-${Date.now()}-${uuidv4()}-${i + 1}.webp`;
-        const buffer = await sharp(image.buffer)
-            .toFormat("webp")
-            .webp({ quality: 95 })
-            .toBuffer();
-        const uploadResult = await new Promise((resolve, reject) => {
-            cloudinarysetup.uploader
-                .upload_stream({ public_id: fileName, resource_type: "image" }, (error, result) => {
-                if (error) {
-                    console.error("Cloudinary upload error: ", error);
-                    return reject(AppError("Image upload failed", 500));
-                }
-                req.body.image.push(result?.secure_url);
-                req.body.imagePublicIds.push(result?.public_id);
-                console.log(req.body);
-                resolve(result);
-            })
-                .end(buffer);
-        });
-        return uploadResult;
-    }));
-    next();
+  const files = req.files;
+  if (!files || !files.image) {
+    return next();
+  }
+  req.body.image = [];
+  req.body.imagePublicIds = [];
+  await Promise.all(
+    files.image.map(async (image, i) => {
+      const fileName = `post-${req.user.id}-${Date.now()}-${uuidv4()}-${
+        i + 1
+      }.webp`;
+      const buffer = await sharp(image.buffer)
+        .toFormat("webp")
+        .webp({ quality: 95 })
+        .toBuffer();
+      const uploadResult = await new Promise((resolve, reject) => {
+        cloudinarysetup.uploader
+          .upload_stream(
+            { public_id: fileName, resource_type: "image" },
+            (error, result) => {
+              if (error) {
+                console.error("Cloudinary upload error: ", error);
+                return reject(AppError("Image upload failed", 500));
+              }
+              req.body.image.push(result?.secure_url);
+              req.body.imagePublicIds.push(result?.public_id);
+              console.log(req.body);
+              resolve(result);
+            },
+          )
+          .end(buffer);
+      });
+      return uploadResult;
+    }),
+  );
+  next();
 });
--- backend/dist/backend/utils/multerSingleImage.js
+++ backend/dist/backend/utils/multerSingleImage.js
@@ -1,85 +1,89 @@
 import dotenv from "dotenv";
 dotenv.config();
 import multer from "multer";
-import { AppError } from "./appError";
-import { catchAsync } from "./catchAsync";
+import { AppError } from "./appError.js";
+import { catchAsync } from "./catchAsync.js";
 import sharp from "sharp";
-import cloudinarysetup from "./cloudinary";
-import { User } from "../model/userModel";
+import cloudinarysetup from "./cloudinary.js";
+import { User } from "../model/userModel.js";
 const multerStorage = multer.memoryStorage();
 const multerFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith("image")) {
-        cb(null, true);
-    }
-    else {
-        cb(AppError("Please upload an image file only", 400), false);
-    }
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(AppError("Please upload an image file only", 400), false);
+  }
 };
 const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
 export const uploadImage = upload.fields([
-    { name: "profileImage", maxCount: 1 },
-    { name: "bannerImage", maxCount: 1 },
+  { name: "profileImage", maxCount: 1 },
+  { name: "bannerImage", maxCount: 1 },
 ]);
 export const resizeAndUploadImages = catchAsync(async (req, res, next) => {
-    if (!req.user) {
-        return next(AppError("Please login", 401));
+  if (!req.user) {
+    return next(AppError("Please login", 401));
+  }
+  const userData = await User.findById(req.user.id);
+  console.log(userData, "userData");
+  if (!userData) {
+    return next(AppError("no user found", 404));
+  }
+  console.log(req.user, "req.user.bannerImagePublicId");
+  const files = req.files;
+  // Function to upload image to Cloudinary and return public_id
+  const uploadToCloudinary = async (file) => {
+    if (!file) {
+      throw AppError("File not found", 400);
     }
-    const userData = await User.findById(req.user.id);
-    console.log(userData, "userData");
-    if (!userData) {
-        return next(AppError("no user found", 404));
+    const buffer = await sharp(file.buffer)
+      .toFormat("webp")
+      .webp({ quality: 100 })
+      .toBuffer();
+    return new Promise((resolve, reject) => {
+      cloudinarysetup.uploader
+        .upload_stream({ resource_type: "image" }, (error, result) => {
+          if (error) {
+            console.error("Cloudinary upload error: ", error);
+            return reject(AppError("Image upload failed", 500));
+          }
+          if (!result) {
+            return reject(
+              AppError("Cloudinary upload failed: result is undefined", 500),
+            );
+          }
+          resolve(result.public_id);
+        })
+        .end(buffer);
+    });
+  };
+  try {
+    // Handle profile image upload
+    if (files.profileImage && files.profileImage.length > 0) {
+      const profileImagePublicId = await uploadToCloudinary(
+        files.profileImage[0],
+      );
+      req.body.profileImage = files.profileImage[0].path; // Set URL or path to profile image
+      req.body.profileImagePublicId = profileImagePublicId;
+      // Optionally delete old profile image if it exists in Cloudinary
+      if (userData.profileImagePublicId) {
+        await cloudinarysetup.uploader.destroy(userData.profileImagePublicId);
+      }
     }
-    console.log(req.user, "req.user.bannerImagePublicId");
-    const files = req.files;
-    // Function to upload image to Cloudinary and return public_id
-    const uploadToCloudinary = async (file) => {
-        if (!file) {
-            throw AppError("File not found", 400);
-        }
-        const buffer = await sharp(file.buffer)
-            .toFormat("webp")
-            .webp({ quality: 100 })
-            .toBuffer();
-        return new Promise((resolve, reject) => {
-            cloudinarysetup.uploader
-                .upload_stream({ resource_type: "image" }, (error, result) => {
-                if (error) {
-                    console.error("Cloudinary upload error: ", error);
-                    return reject(AppError("Image upload failed", 500));
-                }
-                if (!result) {
-                    return reject(AppError("Cloudinary upload failed: result is undefined", 500));
-                }
-                resolve(result.public_id);
-            })
-                .end(buffer);
-        });
-    };
-    try {
-        // Handle profile image upload
-        if (files.profileImage && files.profileImage.length > 0) {
-            const profileImagePublicId = await uploadToCloudinary(files.profileImage[0]);
-            req.body.profileImage = files.profileImage[0].path; // Set URL or path to profile image
-            req.body.profileImagePublicId = profileImagePublicId;
-            // Optionally delete old profile image if it exists in Cloudinary
-            if (userData.profileImagePublicId) {
-                await cloudinarysetup.uploader.destroy(userData.profileImagePublicId);
-            }
-        }
-        // Handle banner image upload
-        if (files.bannerImage && files.bannerImage.length > 0) {
-            const bannerImagePublicId = await uploadToCloudinary(files.bannerImage[0]);
-            req.body.bannerImage = files.bannerImage[0].path; // Set URL or path to banner image
-            req.body.bannerImagePublicId = bannerImagePublicId;
-            // Optionally delete old banner image if it exists in Cloudinary
-            if (userData.bannerImagePublicId) {
-                await cloudinarysetup.uploader.destroy(userData.bannerImagePublicId);
-            }
-        }
-        next();
+    // Handle banner image upload
+    if (files.bannerImage && files.bannerImage.length > 0) {
+      const bannerImagePublicId = await uploadToCloudinary(
+        files.bannerImage[0],
+      );
+      req.body.bannerImage = files.bannerImage[0].path; // Set URL or path to banner image
+      req.body.bannerImagePublicId = bannerImagePublicId;
+      // Optionally delete old banner image if it exists in Cloudinary
+      if (userData.bannerImagePublicId) {
+        await cloudinarysetup.uploader.destroy(userData.bannerImagePublicId);
+      }
     }
-    catch (err) {
-        console.error("Error uploading images:", err);
-        return next(AppError("Failed to upload images", 500));
-    }
+    next();
+  } catch (err) {
+    console.error("Error uploading images:", err);
+    return next(AppError("Failed to upload images", 500));
+  }
 });

5d47ea440b6dfa1bafca30e1e66b52a25cfce3cc
mejat9806 <110531287+mejat9806@users.noreply.github.com>
2024-07-09 13:21:30 +0800

    add delete authRoute

--- backend/dist/backend/routes/authRoutes.js
+++ backend/dist/backend/routes/authRoutes.js
@@ -1,8 +1,21 @@
 import { Router } from "express";
-import { loginUser, registerUser, logout, updateMe, forgotPassword, resetPassword, updatePassword, isLogin, } from "../controller/authController.js";
+import {
+  loginUser,
+  registerUser,
+  logout,
+  updateMe,
+  forgotPassword,
+  resetPassword,
+  updatePassword,
+  isLogin,
+  deleteAccount,
+} from "../controller/authController.js";
 import { verifyJWT } from "../middleware/verifyToken.js";
 import { validateEmail } from "../controller/validationApi.js";
-import { resizeAndUploadImages, uploadImage, } from "../utils/multerSingleImage.js";
+import {
+  resizeAndUploadImages,
+  uploadImage,
+} from "../utils/multerSingleImage.js";
 export const router = Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
@@ -12,5 +25,7 @@ router.post("/forgotPassword", forgotPassword);
 router.patch("/resetPassword/:token", resetPassword);
 router.use(verifyJWT);
 router.get("/isLogin", isLogin);
+router.delete("/deleteAccount/:userId", deleteAccount);
+
 router.patch("/updateMe", uploadImage, resizeAndUploadImages, updateMe);
 router.patch("/updatePassword", updatePassword);
